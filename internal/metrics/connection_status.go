5c2201a96e6e62cacf4d55c271ca81eb66cb2877
Trent Vigar <trent@mydecisive.ai>
2026-04-03 11:37:37 -0400

    feat: ENG-1171 - data connection status endpoint (#92)

---
+++ internal/metrics/connection_status.go
@@ -0,0 +1,176 @@
+package metrics
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"time"
+
+	"github.com/mydecisive/mdai-gateway/internal/telemetry"
+	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+	"github.com/samber/lo"
+	"go.uber.org/zap"
+)
+
+type IngressEgress int
+
+const (
+	Ingress IngressEgress = iota
+	Egress
+)
+
+const (
+	fidelityCheckFail = "fail"
+	fidelityCheckPass = "pass"
+
+	fidelityMetricResult = "result"
+	fidelityMetricSignal = "signal"
+
+	tenMinutes = 10 * time.Minute
+)
+
+type ConnectionStatus struct {
+	promClient promv1.API
+	logger     *zap.Logger
+}
+
+func NewConnectionStatus(promClient promv1.API, logger *zap.Logger) *ConnectionStatus {
+	return &ConnectionStatus{
+		promClient: promClient,
+		logger:     logger,
+	}
+}
+
+func (cs *ConnectionStatus) VerifyDataFidelity(ctx context.Context, telemetryTypes []telemetry.MLT) (bool, error) {
+	// compare now to 10 minutes ago
+	// cutting down on # of queries by getting all labels and filtering here.
+	results, _, err := cs.promClient.QueryRange(ctx, "mdai_fidelity_required_signal_checks_total", promv1.Range{
+		Start: time.Now().Add(-tenMinutes),
+		End:   time.Now(),
+		Step:  tenMinutes,
+	})
+	if err != nil {
+		return false, fmt.Errorf("failed to query prometheus: %w", err)
+	}
+	if results == nil {
+		return false, nil
+	}
+
+	resultMatrix, ok := results.(model.Matrix)
+	if !ok {
+		return false, errors.New("failed to convert result to model.Matrix")
+	}
+
+	for _, telemetryType := range telemetryTypes {
+		if !dataFidelityCheck(cs.logger, resultMatrix, telemetryType) {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func dataFidelityCheck(logger *zap.Logger, resultMatrix model.Matrix, telemetryType telemetry.MLT) bool {
+	failed := lo.Filter(resultMatrix, func(item *model.SampleStream, _ int) bool {
+		return item.Metric[fidelityMetricResult] == fidelityCheckFail &&
+			string(item.Metric[fidelityMetricSignal]) == string(telemetryType)
+	})
+	passed := lo.Filter(resultMatrix, func(item *model.SampleStream, _ int) bool {
+		return item.Metric[fidelityMetricResult] == fidelityCheckPass &&
+			string(item.Metric[fidelityMetricSignal]) == string(telemetryType)
+	})
+
+	// sanity check... this shouldn't happen.
+	if len(failed) != 1 || len(passed) != 1 {
+		logger.Warn("unable to perform data fidelity check, expected 1 set of failed and passed fidelity metric values")
+		return false
+	}
+
+	// if the fidelity check failures are increasing OR the passed fidelity checks are NOT increasing, fail fast
+	if areSeriesValuesIncreasing(failed[0]) || !areSeriesValuesIncreasing(passed[0]) {
+		return false
+	}
+	return true
+}
+
+func (cs *ConnectionStatus) IsTelemetryFlowing(ctx context.Context, ie IngressEgress, telemetryTypes []telemetry.MLT) (bool, error) {
+	for _, connectionType := range telemetryTypes {
+		var promQuery string
+		switch connectionType {
+		case telemetry.Logs:
+			promQuery = lo.Ternary(
+				ie == Ingress,
+				"otelcol_receiver_accepted_log_records_total{receiver=\"datadog\", job=\"otel-collector\"}",
+				"otelcol_exporter_sent_log_records{receiver=\"datadog\", job=\"otel-collector\"}",
+			)
+		case telemetry.Traces:
+			promQuery = lo.Ternary(
+				ie == Ingress,
+				"otelcol_receiver_accepted_spans_total{receiver=\"datadog\", job=\"otel-collector\"}",
+				"otelcol_exporter_sent_spans{receiver=\"datadog\", job=\"otel-collector\"}",
+			)
+		case telemetry.Metrics:
+			promQuery = lo.Ternary(
+				ie == Ingress,
+				"otelcol_receiver_accepted_metric_points_total{receiver=\"datadog\", job=\"otel-collector\"}",
+				"otelcol_exporter_sent_metric_points{receiver=\"datadog\", job=\"otel-collector\"}",
+			)
+		default:
+			return false, fmt.Errorf("unknown telemetry type: %s", connectionType)
+		}
+
+		// TODO: figure out how to query a label to get EXACTLY the collector we want to look at, don't want to sum across multiple collectors
+		// compare the last minute of results
+		results, _, err := cs.promClient.QueryRange(ctx, promQuery, promv1.Range{
+			Start: time.Now().Add(-1 * time.Minute),
+			End:   time.Now(),
+			Step:  time.Minute,
+		})
+		if err != nil {
+			return false, fmt.Errorf("failed to query prometheus: %w", err)
+		}
+
+		var metricsIncreasing bool
+		metricsIncreasing, err = areMatrixValuesIncreasing(results)
+		if err != nil {
+			return false, fmt.Errorf("analyzing query range results: %w", err)
+		}
+
+		// return immediately if one of the telemetry types isn't increasing, we don't need to keep checking
+		if !metricsIncreasing {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func areMatrixValuesIncreasing(results model.Value) (bool, error) {
+	if results == nil {
+		return false, nil
+	}
+	resultMatrix, ok := results.(model.Matrix)
+	if !ok {
+		return false, errors.New("failed to convert input result to expected type")
+	}
+
+	if slices.ContainsFunc(resultMatrix, areSeriesValuesIncreasing) {
+		return true, nil
+	}
+	return false, nil
+}
+
+func areSeriesValuesIncreasing(series *model.SampleStream) bool {
+	for i := 1; i < len(series.Values); i++ {
+		prev := series.Values[i-1]
+		curr := series.Values[i]
+
+		diff := float64(curr.Value) - float64(prev.Value)
+
+		if diff > 0 {
+			// we can return immediately if the values went up in our time range, no need to keep going.
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/metrics/connection_status_test.go
@@ -0,0 +1,603 @@
+package metrics
+
+import (
+	"testing"
+
+	v1mock "github.com/mydecisive/mdai-gateway/internal/mock/v1"
+	"github.com/mydecisive/mdai-gateway/internal/telemetry"
+	"github.com/mydecisive/mdai-gateway/internal/test"
+	"github.com/prometheus/common/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
+)
+
+var (
+	failingFidelityResults = model.Matrix{
+		{
+			// failures ARE increasing
+			Metric: model.Metric{
+				fidelityMetricResult: fidelityCheckFail,
+				fidelityMetricSignal: model.LabelValue(telemetry.Traces),
+			},
+			Values: []model.SamplePair{
+				{
+					Value: model.SampleValue(1.0),
+				},
+				{
+					Value: model.SampleValue(2.0),
+				},
+			},
+		},
+		{
+			Metric: model.Metric{
+				fidelityMetricResult: fidelityCheckPass,
+				fidelityMetricSignal: model.LabelValue(telemetry.Traces),
+			},
+			Values: []model.SamplePair{
+				{
+					Value: model.SampleValue(1.0),
+				},
+				{
+					Value: model.SampleValue(2.0),
+				},
+			},
+		},
+	}
+
+	passingFidelityResults = model.Matrix{
+		{
+			// failures are NOT increasing
+			Metric: model.Metric{
+				fidelityMetricResult: fidelityCheckFail,
+				fidelityMetricSignal: model.LabelValue(telemetry.Traces),
+			},
+			Values: []model.SamplePair{
+				{
+					Value: model.SampleValue(1.0),
+				},
+				{
+					Value: model.SampleValue(1.0),
+				},
+			},
+		},
+		{
+			// passes ARE increasing
+			Metric: model.Metric{
+				fidelityMetricResult: fidelityCheckPass,
+				fidelityMetricSignal: model.LabelValue(telemetry.Traces),
+			},
+			Values: []model.SamplePair{
+				{
+					Value: model.SampleValue(1.0),
+				},
+				{
+					Value: model.SampleValue(2.0),
+				},
+			},
+		},
+	}
+)
+
+func TestAreSeriesValuesIncreasing(t *testing.T) {
+	t.Parallel()
+
+	t.Run("all decreasing", func(t *testing.T) {
+		t.Parallel()
+
+		areIncreasing := areSeriesValuesIncreasing(&model.SampleStream{
+			Values: []model.SamplePair{
+				{
+					Value: model.SampleValue(1.23),
+				},
+				{
+					Value: model.SampleValue(1.22),
+				},
+				{
+					Value: model.SampleValue(1.21),
+				},
+			},
+		})
+		assert.False(t, areIncreasing)
+	})
+
+	t.Run("has increasing", func(t *testing.T) {
+		t.Parallel()
+
+		areIncreasing := areSeriesValuesIncreasing(&model.SampleStream{
+			Values: []model.SamplePair{
+				{
+					Value: model.SampleValue(1.23),
+				},
+				{
+					Value: model.SampleValue(1.22),
+				},
+				{
+					Value: model.SampleValue(1.23),
+				},
+				{
+					Value: model.SampleValue(0.23),
+				},
+			},
+		})
+		assert.True(t, areIncreasing)
+	})
+}
+
+func TestAreMatrixValuesIncreasing(t *testing.T) {
+	t.Parallel()
+
+	type checkResults func(t *testing.T, actual bool, err error)
+
+	testCases := []struct {
+		description  string
+		inputResults model.Value
+		check        checkResults
+	}{
+		{
+			description:  "nil results",
+			inputResults: nil,
+			check: func(t *testing.T, actual bool, err error) { // nolint: thelper
+				require.False(t, actual)
+				require.NoError(t, err)
+			},
+		},
+		{
+			description: "invalid result type",
+			inputResults: model.Vector{
+				{
+					Value: model.SampleValue(1.23),
+				},
+				{
+					Value: model.SampleValue(23.45),
+				},
+			},
+			check: func(t *testing.T, actual bool, err error) { // nolint: thelper
+				require.False(t, actual)
+				require.ErrorContains(t, err, "failed to convert input result to expected type")
+			},
+		},
+		{
+			description: "not increasing",
+			inputResults: model.Matrix{
+				{
+					Values: []model.SamplePair{
+						{
+							Value: model.SampleValue(1.23),
+						},
+						{
+							Value: model.SampleValue(0.23),
+						},
+					},
+				},
+				{
+					Values: []model.SamplePair{
+						{
+							Value: model.SampleValue(1.23),
+						},
+						{
+							Value: model.SampleValue(1.22),
+						},
+					},
+				},
+			},
+			check: func(t *testing.T, actual bool, err error) { // nolint: thelper
+				require.False(t, actual)
+				require.NoError(t, err)
+			},
+		},
+		{
+			description: "eventually increasing",
+			inputResults: model.Matrix{
+				{
+					Values: []model.SamplePair{
+						{
+							Value: model.SampleValue(1.23),
+						},
+						{
+							Value: model.SampleValue(0.23),
+						},
+					},
+				},
+				{
+					Values: []model.SamplePair{
+						{
+							Value: model.SampleValue(1.23),
+						},
+						{
+							Value: model.SampleValue(1.24),
+						},
+						{
+							Value: model.SampleValue(1.21),
+						},
+					},
+				},
+			},
+			check: func(t *testing.T, actual bool, err error) { // nolint: thelper
+				require.True(t, actual)
+				require.NoError(t, err)
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := areMatrixValuesIncreasing(testCase.inputResults)
+			testCase.check(t, actual, err)
+		})
+	}
+}
+
+func TestIsTelemetryFlowing(t *testing.T) {
+	t.Parallel()
+
+	t.Run("error - invalid MLT type", func(t *testing.T) {
+		t.Parallel()
+
+		mockPromAPI := v1mock.NewMockAPI(t)
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+
+		actual, err := theThing.IsTelemetryFlowing(t.Context(), Ingress, []telemetry.MLT{"invalid", telemetry.Logs, telemetry.Traces})
+		require.False(t, actual)
+		require.ErrorContains(t, err, "unknown telemetry type: invalid")
+	})
+
+	t.Run("error querying prometheus API", func(t *testing.T) {
+		t.Parallel()
+
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_log_records_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(nil, nil, assert.AnError).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+
+		actual, err := theThing.IsTelemetryFlowing(t.Context(), Ingress, []telemetry.MLT{telemetry.Logs, telemetry.Traces})
+		require.False(t, actual)
+		require.ErrorContains(t, err, "failed to query prometheus")
+	})
+
+	t.Run("error - invalid query results", func(t *testing.T) {
+		t.Parallel()
+
+		queryResults := model.Vector{
+			{
+				Value: model.SampleValue(1.23),
+			},
+			{
+				Value: model.SampleValue(1.32),
+			},
+		}
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_log_records_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(queryResults, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+
+		actual, err := theThing.IsTelemetryFlowing(t.Context(), Ingress, []telemetry.MLT{telemetry.Logs, telemetry.Traces})
+		require.False(t, actual)
+		require.ErrorContains(t, err, "analyzing query range results")
+	})
+
+	t.Run("happy path - not increasing", func(t *testing.T) {
+		t.Parallel()
+
+		logsResults := model.Matrix{
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(2.23),
+					},
+				},
+			},
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(1.24),
+					},
+				},
+			},
+		}
+		tracesResults := model.Matrix{
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(1.21),
+					},
+				},
+			},
+		}
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_log_records_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(logsResults, nil, nil).
+			Times(1)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_spans_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(tracesResults, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+
+		actual, err := theThing.IsTelemetryFlowing(t.Context(), Ingress, []telemetry.MLT{telemetry.Logs, telemetry.Traces})
+		require.False(t, actual)
+		require.NoError(t, err)
+	})
+
+	t.Run("happy path - increasing", func(t *testing.T) {
+		t.Parallel()
+
+		logsResults := model.Matrix{
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(2.23),
+					},
+				},
+			},
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(1.22),
+					},
+				},
+			},
+		}
+		tracesResults := model.Matrix{
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(0.23),
+					},
+				},
+			},
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(1.24),
+					},
+				},
+			},
+		}
+		metricsResults := model.Matrix{
+			{
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.23),
+					},
+					{
+						Value: model.SampleValue(2.23),
+					},
+				},
+			},
+		}
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_log_records_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(logsResults, nil, nil).
+			Times(1)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_spans_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(tracesResults, nil, nil).
+			Times(1)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "otelcol_receiver_accepted_metric_points_total{receiver=\"datadog\", job=\"otel-collector\"}", mock.Anything).
+			Return(metricsResults, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+
+		actual, err := theThing.IsTelemetryFlowing(t.Context(), Ingress, []telemetry.MLT{telemetry.Logs, telemetry.Traces, telemetry.Metrics})
+		require.True(t, actual)
+		require.NoError(t, err)
+	})
+}
+
+func TestDataFidelityCheck(t *testing.T) {
+	t.Parallel()
+
+	t.Run("error - multiple failed metrics", func(t *testing.T) {
+		t.Parallel()
+
+		fidelityResults := model.Matrix{
+			{
+				// failures are NOT increasing
+				Metric: model.Metric{
+					fidelityMetricResult: fidelityCheckFail,
+					fidelityMetricSignal: model.LabelValue(telemetry.Traces),
+					"someLabel":          "abc123",
+				},
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.0),
+					},
+					{
+						Value: model.SampleValue(1.0),
+					},
+				},
+			},
+			{
+				// passes ARE increasing
+				Metric: model.Metric{
+					fidelityMetricResult: fidelityCheckFail,
+					fidelityMetricSignal: model.LabelValue(telemetry.Traces),
+					"someLabel":          "xyz999",
+				},
+				Values: []model.SamplePair{
+					{
+						Value: model.SampleValue(1.0),
+					},
+					{
+						Value: model.SampleValue(2.0),
+					},
+				},
+			},
+		}
+
+		testLogger, logOutput := test.NewTestLogger(t, zapcore.InfoLevel)
+		passedCheck := dataFidelityCheck(testLogger, fidelityResults, telemetry.Traces)
+		require.False(t, passedCheck)
+
+		// validate log output
+		test.ValidateOutput(t, logOutput, []test.LogOutput{
+			{
+				Level:   zapcore.WarnLevel,
+				Message: "unable to perform data fidelity check, expected 1 set of failed and passed fidelity metric values",
+			},
+		})
+	})
+
+	t.Run("happy path - data fidelity is NOT good", func(t *testing.T) {
+		t.Parallel()
+
+		testLogger, _ := test.NewTestLogger(t, zapcore.InfoLevel)
+		passedCheck := dataFidelityCheck(testLogger, failingFidelityResults, telemetry.Traces)
+		require.False(t, passedCheck)
+	})
+
+	t.Run("happy path - data fidelity is good", func(t *testing.T) {
+		t.Parallel()
+
+		testLogger, _ := test.NewTestLogger(t, zapcore.InfoLevel)
+		passedCheck := dataFidelityCheck(testLogger, passingFidelityResults, telemetry.Traces)
+		require.True(t, passedCheck)
+	})
+}
+
+func TestVerifyDataFidelity(t *testing.T) {
+	t.Parallel()
+
+	t.Run("error - unexpected query error", func(t *testing.T) {
+		t.Parallel()
+
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "mdai_fidelity_required_signal_checks_total", mock.Anything).
+			Return(nil, nil, assert.AnError).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+		result, err := theThing.VerifyDataFidelity(t.Context(), []telemetry.MLT{telemetry.Logs})
+		require.ErrorContains(t, err, "failed to query prometheus")
+		require.False(t, result)
+	})
+
+	t.Run("nil query results", func(t *testing.T) {
+		t.Parallel()
+
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "mdai_fidelity_required_signal_checks_total", mock.Anything).
+			Return(nil, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+		result, err := theThing.VerifyDataFidelity(t.Context(), []telemetry.MLT{telemetry.Logs})
+		require.NoError(t, err)
+		require.False(t, result)
+	})
+
+	t.Run("error - invalid prometheus query result", func(t *testing.T) {
+		t.Parallel()
+
+		invalidResults := model.Vector{}
+
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "mdai_fidelity_required_signal_checks_total", mock.Anything).
+			Return(invalidResults, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+		result, err := theThing.VerifyDataFidelity(t.Context(), []telemetry.MLT{telemetry.Logs})
+		require.ErrorContains(t, err, "failed to convert result to model.Matrix")
+		require.False(t, result)
+	})
+
+	t.Run("happy path - data fidelity is NOT good", func(t *testing.T) {
+		t.Parallel()
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "mdai_fidelity_required_signal_checks_total", mock.Anything).
+			Return(failingFidelityResults, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+		result, err := theThing.VerifyDataFidelity(t.Context(), []telemetry.MLT{telemetry.Traces})
+		require.NoError(t, err)
+		require.False(t, result)
+	})
+
+	t.Run("happy path - data fidelity is good", func(t *testing.T) {
+		t.Parallel()
+
+		mockPromAPI := v1mock.NewMockAPI(t)
+		mockPromAPI.EXPECT().
+			QueryRange(mock.Anything, "mdai_fidelity_required_signal_checks_total", mock.Anything).
+			Return(passingFidelityResults, nil, nil).
+			Times(1)
+
+		theThing := &ConnectionStatus{
+			logger:     zaptest.NewLogger(t),
+			promClient: mockPromAPI,
+		}
+		result, err := theThing.VerifyDataFidelity(t.Context(), []telemetry.MLT{telemetry.Traces})
+		require.NoError(t, err)
+		require.True(t, result)
+	})
+}
