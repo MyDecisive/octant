9d3fd2561002067f93eb13f120bda2d0f0908dfd
Trent Vigar <trent@mydecisive.ai>
2026-04-21 10:25:47 -0400

    ENG-1126 implemented testing the argo cd connection

--- internal/rpc/handler/argocd.go
+++ internal/rpc/handler/argocd.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	octantv1alpha "github.com/MyDecisive/octant-contracts/go/pkg/octant/v1alpha"
 	"github.com/MyDecisive/octant-contracts/go/pkg/octant/v1alpha/octantv1alphaconnect"
+	"github.com/argoproj/argo-cd/v3/pkg/apiclient"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"connectrpc.com/connect"
@@ -20,15 +21,51 @@ func NewArgoCDHandler() *ArgoCDHandler {
 }
 
 func (ah *ArgoCDHandler) TestConnection(
-	_ context.Context,
+	ctx context.Context,
 	req *connect.Request[octantv1alpha.TestConnectionRequest],
 ) (*connect.Response[octantv1alpha.TestConnectionResponse], error) {
 	argoEndpoint := req.Msg.GetArgoEndpoint()
-	_ = req.Msg.GetArgoAccountToken()
+	argoAccountToken := req.Msg.GetArgoAccountToken()
 
 	logger := zap.L().With(zap.String("argoEndpoint", argoEndpoint))
 
 	logger.Debug("received test connection request")
+
+	argoClient, err := apiclient.NewClient(&apiclient.ClientOptions{
+		ServerAddr: argoEndpoint,
+		AuthToken:  argoAccountToken,
+	})
+	if err != nil {
+		logger.Error("creating argo api client", zap.Error(err))
+		return &connect.Response[octantv1alpha.TestConnectionResponse]{
+			Msg: &octantv1alpha.TestConnectionResponse{
+				Success: false,
+			},
+		}, nil
+	}
+
+	closer, versionClient, err := argoClient.NewVersionClient()
+	if err != nil {
+		logger.Error("creating argo version client", zap.Error(err))
+		return &connect.Response[octantv1alpha.TestConnectionResponse]{
+			Msg: &octantv1alpha.TestConnectionResponse{
+				Success: false,
+			},
+		}, nil
+	}
+	defer closer.Close()
+
+	// to validate the argocd account token, we'll just query the server version, which will validate our account token.
+	_, err = versionClient.Version(ctx, nil)
+	if err != nil {
+		logger.Error("getting argo version", zap.Error(err))
+		return &connect.Response[octantv1alpha.TestConnectionResponse]{
+			Msg: &octantv1alpha.TestConnectionResponse{
+				Success: false,
+			},
+		}, nil
+	}
+
 	return &connect.Response[octantv1alpha.TestConnectionResponse]{
 		Msg: &octantv1alpha.TestConnectionResponse{
 			Success: true,
