addf5b1b7b21f42d636fd400a6522c761af03d33
xhsun <xhsun@users.noreply.github.com>
2026-04-23 18:19:18 +0000

    DI

---
+++ internal/registry/startWithWeb.go
@@ -0,0 +1,34 @@
+//go:build webapp
+// +build webapp
+
+package registry
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mydecisive/octant/internal/rpc"
+	"github.com/mydecisive/octant/web"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+)
+
+func Start(rpcServer *rpc.Server) error {
+	ui, err := web.CreateServer()
+	if err != nil {
+		return fmt.Errorf("ui server: %w", err)
+	}
+
+	// Init Servers
+	g, _ := errgroup.WithContext(context.Background())
+
+	// Start servers
+	g.Go(func() error {
+		return fmt.Errorf("rpc server: %w", rpcServer.Start())
+	})
+	g.Go(func() error {
+		return fmt.Errorf("UI server: %w", ui.ListenAndServe())
+	})
+	zap.L().Info("Start all servers")
+	return g.Wait()
+}
--- web/web.go
+++ web/web.go
@@ -3,7 +3,47 @@
 
 package web
 
-import "embed"
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"time"
+
+	"github.com/mydecisive/mdai-data-core/helpers"
+)
 
 //go:embed dist/*
 var App embed.FS
+
+const (
+	httpPortEnvVarKey = "HTTP_PORT"
+	defaultHTTPPort   = "5678"
+
+	defaultReadHeaderTimeout = 5 * time.Second
+	defaultReadTimeout       = 10 * time.Second
+	defaultWriteTimeout      = 10 * time.Second
+	defaultIdleTimeout       = 120 * time.Second
+)
+
+func CreateServer() (*http.Server, error) {
+	mainRouter := http.NewServeMux()
+
+	octantApp, err := fs.Sub(App, "dist")
+	if err != nil {
+		return nil, err
+	}
+
+	// octant UI
+	mainRouter.Handle("/", http.FileServerFS(octantApp))
+
+	httpPort := helpers.GetEnvVariableWithDefault(httpPortEnvVarKey, defaultHTTPPort)
+
+	return &http.Server{
+		Addr:              ":" + httpPort,
+		Handler:           mainRouter,
+		ReadHeaderTimeout: defaultReadHeaderTimeout,
+		ReadTimeout:       defaultReadTimeout,
+		WriteTimeout:      defaultWriteTimeout,
+		IdleTimeout:       defaultIdleTimeout,
+	}, nil
+}
