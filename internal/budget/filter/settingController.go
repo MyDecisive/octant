f2585433761f1da2058e675a15cf3ad5cf43f789
xhsun <xhsun@users.noreply.github.com>
2026-04-23 21:35:41 +0000

    update

--- internal/budget/filter/settingController.go
+++ internal/budget/filter/settingController.go
@@ -10,9 +10,21 @@ import (
 
 	budgetv1alpha "github.com/MyDecisive/octant-contracts/go/pkg/budget/v1alpha"
 	"github.com/mydecisive/octant/internal/config"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 )
+
+// SettingController provide abilities to get and set the budget filter settings.
+type SettingController interface {
+	// GetFilter returns filter setting of the given type.
+	// If an update is in progress for the given type, this will return `ErrStillUpdating`.
+	GetFilter(filterType budgetv1alpha.FilterType, namespace string, connection string) (*budgetv1alpha.Filter, error)
+	// UpdateFilter updates the filter setting of the given type with the provided values.
+	// If an update is already in progress for the given type, this will return `ErrStillUpdating`.
+	// If the update takes longer than the timeout, this will return `ErrTimeout`.
+	UpdateFilter(ctx context.Context, namespace string, connection string, updates *budgetv1alpha.Filter) error
+}
 
 var (
 	ErrStillUpdating = errors.New("still updating")
@@ -22,89 +34,153 @@ var (
 )
 
 const (
-	collectorLogNameFormatter   = "%s-log-sampling"
-	collectorTraceNameFormatter = "%s-trace-sampling"
-	varLogsRatioNumber          = "logs_ratio_number"
-	varLogsPersistErrors        = "logs_persist_errors"
-	varTracesRatioNumber        = "traces_ratio_number"
-	varTracesPersistErrors      = "traces_persist_errors"
+	// collectorLogNameFormatter used to generate the name of the OTEL collector
+	// that is responsible for applying the log filters.
+	// Used by the update process to wait for the update to complete.
+	collectorLogNameFormatter = "%s-log-sampling-collector"
+	// collectorTraceNameFormatter used to generate the name of the OTEL collector
+	// that is responsible for applying the trace filters.
+	// Used by the update process to wait for the update to complete.
+	collectorTraceNameFormatter = "%s-trace-sampling-collector"
+	// varLogsRatioNumber contains the hub manual variable name that corresponds to log PctSampled.
+	varLogsRatioNumber = "logs_ratio_number"
+	// varLogsPersistErrors contains the hub manual variable name that corresponds to log IncludeErr.
+	varLogsPersistErrors = "logs_persist_errors"
+	// varTracesRatioNumber contains the hub manual variable name that corresponds to trace PctSampled.
+	varTracesRatioNumber = "traces_ratio_number"
+	// varTracesPersistErrors contains the hub manual variable name that corresponds to trace IncludeErr.
+	varTracesPersistErrors = "traces_persist_errors"
 )
 
-type SettingController struct {
-	log           *sync.Mutex
-	trace         *sync.Mutex
-	accessor      VariableAccessor
-	kube          kubernetes.Interface
-	configuration config.Configuration
+// settingInput used by the unexported methods to pass values.
+type settingInput struct {
+	namespace    string
+	connection   string
+	ratioVarName string
+	errorVarName string
 }
 
-func NewSettingController(configuration config.Configuration, accessor VariableAccessor, kube kubernetes.Interface) *SettingController {
-	return &SettingController{
-		log:           new(sync.Mutex),
-		trace:         new(sync.Mutex),
+// MDAISettingController implements SettingController.
+type MDAISettingController struct {
+	log           *sync.RWMutex
+	trace         *sync.RWMutex
+	accessor      VariableAccessor
+	kube          kubernetes.Interface
+	configuration *config.Configuration
+}
+
+// Ensure MDAISettingController implements SettingController.
+var _ SettingController = &MDAISettingController{}
+
+// NewMDAISettingController returns a new instance of MDAISettingController.
+func NewMDAISettingController(configuration *config.Configuration, accessor VariableAccessor, kube kubernetes.Interface) *MDAISettingController {
+	return &MDAISettingController{
+		log:           new(sync.RWMutex),
+		trace:         new(sync.RWMutex),
 		accessor:      accessor,
 		kube:          kube,
 		configuration: configuration,
 	}
 }
 
-func (sc *SettingController) GetFilter(filterType budgetv1alpha.FilterType, namespace string, connectionName string) (*budgetv1alpha.Filter, error) {
-	if !sc.log.TryLock() {
-		return nil, ErrStillUpdating
-	}
-
-	variables, err := sc.accessor.GetAllVariables(namespace, connectionName)
-	if err != nil {
-		return nil, err
+// GetFilter returns filter setting of the given type.
+// If an update is in progress for the given type, this will return `ErrStillUpdating`.
+func (sc *MDAISettingController) GetFilter(filterType budgetv1alpha.FilterType, namespace string, connection string) (*budgetv1alpha.Filter, error) {
+	input := settingInput{
+		namespace:  namespace,
+		connection: connection,
 	}
 
 	switch filterType {
 	case budgetv1alpha.FilterType_FILTER_TYPE_LOG:
-		return sc.parseFilterSetting(variables, varLogsRatioNumber, varLogsPersistErrors)
+		input.ratioVarName = varLogsRatioNumber
+		input.errorVarName = varLogsPersistErrors
+		if !sc.log.TryRLock() {
+			return nil, ErrStillUpdating
+		}
+		defer sc.log.RUnlock()
 	case budgetv1alpha.FilterType_FILTER_TYPE_TRACE:
-		return sc.parseFilterSetting(variables, varTracesRatioNumber, varTracesPersistErrors)
+		input.ratioVarName = varTracesRatioNumber
+		input.errorVarName = varTracesPersistErrors
+		if !sc.trace.TryRLock() {
+			return nil, ErrStillUpdating
+		}
+		defer sc.trace.RUnlock()
 	default:
 		return nil, ErrInvalid
 	}
+	return sc.getFilter(input)
 }
 
-func (sc *SettingController) parseFilterSetting(variables map[string]string, ratioVarName string, errorVarName string) (*budgetv1alpha.Filter, error) {
+// UpdateFilter updates the filter setting of the given type with the provided values.
+// If an update is already in progress for the given type, this will return `ErrStillUpdating`.
+// If the update takes longer than the timeout, this will return `ErrTimeout`.
+func (sc *MDAISettingController) UpdateFilter(ctx context.Context, namespace string, connection string, updates *budgetv1alpha.Filter) error {
+	var collectorFormatter string
+	input := settingInput{
+		namespace:  namespace,
+		connection: connection,
+	}
+
+	switch updates.Type {
+	case budgetv1alpha.FilterType_FILTER_TYPE_LOG:
+		input.ratioVarName = varLogsRatioNumber
+		input.errorVarName = varLogsPersistErrors
+		collectorFormatter = collectorLogNameFormatter
+
+		if !sc.log.TryLock() {
+			return ErrStillUpdating
+		}
+		defer sc.log.Unlock()
+	case budgetv1alpha.FilterType_FILTER_TYPE_TRACE:
+		input.ratioVarName = varTracesRatioNumber
+		input.errorVarName = varTracesPersistErrors
+		collectorFormatter = collectorTraceNameFormatter
+
+		if !sc.trace.TryLock() {
+			return ErrStillUpdating
+		}
+		defer sc.trace.Unlock()
+	default:
+		return ErrInvalid
+	}
+
+	return sc.updateFilter(ctx, input, collectorFormatter, updates)
+}
+
+// getFilter retrieves the filter settings from MDAI gateway and parse them into `budgetv1alpha.Filter`.
+func (sc *MDAISettingController) getFilter(input settingInput) (*budgetv1alpha.Filter, error) {
 	filter := &budgetv1alpha.Filter{}
-	numStr, ok := variables[ratioVarName]
-	if !ok {
-		return nil, fmt.Errorf("%w:%s", ErrNotFound, ratioVarName)
+
+	numStr, err := sc.accessor.GetVariable(input.namespace, input.connection, input.ratioVarName)
+	if err != nil {
+		return nil, fmt.Errorf("%s:%w:%w", input.ratioVarName, ErrNotFound, err)
 	}
 	num, err := strconv.ParseUint(numStr, 10, 32)
 	if err != nil {
-		return nil, fmt.Errorf("%w:%s", ErrInvalid, ratioVarName)
+		return nil, fmt.Errorf("%w:%s", ErrInvalid, input.ratioVarName)
 	}
 	filter.PctSampled = uint32(num)
 
-	boolStr, ok := variables[errorVarName]
-	if !ok {
-		return nil, fmt.Errorf("%w:%s", ErrNotFound, errorVarName)
+	boolStr, err := sc.accessor.GetVariable(input.namespace, input.connection, input.errorVarName)
+	if err != nil {
+		return nil, fmt.Errorf("%s:%w:%w", input.errorVarName, ErrNotFound, err)
 	}
 	persistErr, err := strconv.ParseBool(boolStr)
 	if err != nil {
-		return nil, fmt.Errorf("%w:%s", ErrInvalid, errorVarName)
+		return nil, fmt.Errorf("%w:%s", ErrInvalid, input.errorVarName)
 	}
 	filter.IncludeErr = persistErr
 
 	return filter, nil
 }
 
-func (sc *SettingController) updateLogFilter(ctx context.Context, updates *budgetv1alpha.Filter, namespace string, connectionName string) error {
-	if !sc.log.TryLock() {
-		return ErrStillUpdating
-	}
-
-	sc.log.Lock()
-	defer sc.log.Unlock()
-
-	if err := sc.accessor.PostVariable(namespace, connectionName, varLogsRatioNumber, strconv.FormatUint(uint64(updates.GetPctSampled()), 10)); err != nil {
+// updateFilter updates the filter settings in MDAI gateway and then wait for the changes to propagate to the collector pod(s).
+func (sc *MDAISettingController) updateFilter(ctx context.Context, input settingInput, collectorNameFormatter string, updates *budgetv1alpha.Filter) error {
+	if err := sc.accessor.UpdateVariable(input.namespace, input.connection, input.ratioVarName, strconv.FormatUint(uint64(updates.GetPctSampled()), 10)); err != nil {
 		return err
 	}
-	if err := sc.accessor.PostVariable(namespace, connectionName, varLogsPersistErrors, updates.GetIncludeErr()); err != nil {
+	if err := sc.accessor.UpdateVariable(input.namespace, input.connection, input.errorVarName, updates.GetIncludeErr()); err != nil {
 		return err
 	}
 
@@ -113,8 +189,11 @@ func (sc *SettingController) updateLogFilter(ctx context.Context, updates *budge
 		time.Duration(sc.configuration.Budget.FilterSettingUpdateTimeout)*time.Second,
 		true,
 		func(ctx context.Context) (done bool, err error) {
-			sc.kube.CoreV1().Pods(namespace).Get(ctx)
-			return false, nil
+			deployment, err := sc.kube.AppsV1().Deployments(input.namespace).Get(ctx, fmt.Sprintf(collectorNameFormatter, input.connection), v1.GetOptions{})
+			if err != nil {
+				return true, err
+			}
+			return deployment.Status.Replicas == deployment.Status.ReadyReplicas, nil
 		}); err != nil {
 		if errors.Is(err, context.DeadlineExceeded) {
 			return ErrTimeout
