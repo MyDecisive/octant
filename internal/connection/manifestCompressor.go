5f1f5958a9383699b1969dc2c6dff9f97a5635ab
xhsun <xhsun@users.noreply.github.com>
2026-04-24 20:49:13 +0000

    compressor

---
+++ internal/connection/manifestCompressor.go
@@ -0,0 +1,104 @@
+package connection
+
+import (
+	"archive/zip"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+
+	octantv1alpha "github.com/MyDecisive/octant-contracts/go/pkg/octant/v1alpha"
+	"github.com/mydecisive/octant/internal/telemetry"
+)
+
+type CompressionInput struct {
+	Namespace      string
+	Connection     string
+	Telemetries    []octantv1alpha.MLTType
+	Format         octantv1alpha.ManifestOutFormat
+	DeploymentType octantv1alpha.DeploymentType
+}
+
+type ManifestCompressor interface {
+	// CreateCompressed creates manifest files abse on the given inputs and then compress the files into a zip.
+	CreateCompressed(ctx context.Context, input CompressionInput) (*bytes.Buffer, error)
+}
+
+type ConnectionManifestCompressor struct {
+}
+
+var _ ManifestCompressor = &ConnectionManifestCompressor{}
+
+func NewConnectionManifestCompressor() *ConnectionManifestCompressor {
+	return &ConnectionManifestCompressor{}
+}
+
+// CreateCompressed creates manifest files abse on the given inputs and then compress the files into a zip.
+func (cmc *ConnectionManifestCompressor) CreateCompressed(ctx context.Context, input CompressionInput) (*bytes.Buffer, error) {
+	manifestsMap, err := CreateExportableArgoManifests(
+		input.Namespace,
+		input.Connection,
+		cmc.toConnectionData(input.Telemetries, input.DeploymentType),
+		cmc.toConnectionFormat(input.Format),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("render manifest:%w", err)
+	}
+
+	buf := new(bytes.Buffer)
+	zipWriter := zip.NewWriter(buf)
+	defer zipWriter.Close() //nolint:errcheck
+	for filename, content := range manifestsMap {
+		select {
+		case <-ctx.Done():
+			return nil, errors.New("context cancelled")
+		default:
+		}
+
+		fWriter, err := zipWriter.Create(filename)
+		if err != nil {
+			return nil, fmt.Errorf("generate zip for %s:%w", filename, err)
+		}
+
+		if _, err := fWriter.Write(content); err != nil {
+			return nil, fmt.Errorf("write zip for %s:%w", filename, err)
+		}
+	}
+	return buf, nil
+}
+
+func (*ConnectionManifestCompressor) toConnectionData(telemetries []octantv1alpha.MLTType, deployment octantv1alpha.DeploymentType) OctantConnectionData {
+	telemetryTypes := make([]telemetry.MLT, len(telemetries))
+	for i, t := range telemetries {
+		switch t {
+		case octantv1alpha.MLTType_MLT_TYPE_METRIC:
+			telemetryTypes[i] = telemetry.Metrics
+		case octantv1alpha.MLTType_MLT_TYPE_LOG:
+			telemetryTypes[i] = telemetry.Logs
+		case octantv1alpha.MLTType_MLT_TYPE_TRACE:
+			telemetryTypes[i] = telemetry.Traces
+		}
+	}
+
+	deploymentType := ArgoSideloadDeploymentType
+	if deployment == octantv1alpha.DeploymentType_DEPLOYMENT_TYPE_ARGO_MANIFEST {
+		deploymentType = ArgoManifestsDeploymentType
+	}
+
+	return OctantConnectionData{
+		TelemetryTypes: telemetryTypes,
+		Deployment: &Deployment{
+			Type: deploymentType,
+		},
+		Destinations: make([]OctantConnectionDestination, 1),
+	}
+}
+
+func (*ConnectionManifestCompressor) toConnectionFormat(format octantv1alpha.ManifestOutFormat) ManifestOutputFormat {
+	result := YAMLOutputFormat
+	if format == octantv1alpha.ManifestOutFormat_MANIFEST_OUT_FORMAT_JSON {
+		result = JSONOutputFormat
+	}
+
+	return result
+}
