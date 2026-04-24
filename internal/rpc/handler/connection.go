9e9b143409e2736c109dddcd51860aeb0a9feb8e
xhsun <xhsun@users.noreply.github.com>
2026-04-24 20:49:19 +0000

    handler

---
+++ internal/rpc/handler/connection.go
@@ -0,0 +1,74 @@
+package rpchandler
+
+import (
+	"context"
+	"errors"
+	"io"
+
+	"connectrpc.com/connect"
+	octantv1alpha "github.com/MyDecisive/octant-contracts/go/pkg/octant/v1alpha"
+	"github.com/MyDecisive/octant-contracts/go/pkg/octant/v1alpha/octantv1alphaconnect"
+	"github.com/mydecisive/octant/internal/config"
+	"github.com/mydecisive/octant/internal/connection"
+	"go.uber.org/zap"
+)
+
+const chunkSize = 1000000 // 1MB
+const manifestContentZip = "application/zip"
+
+type ConnectionHandler struct {
+	octantv1alphaconnect.UnimplementedConnectionServiceHandler
+
+	config     *config.Configuration
+	compressor connection.ManifestCompressor
+}
+
+func NewConnectionHandler(config *config.Configuration, compressor connection.ManifestCompressor) *ConnectionHandler {
+	return &ConnectionHandler{
+		config:     config,
+		compressor: compressor,
+	}
+}
+
+func (ch *ConnectionHandler) GenerateManifests(ctx context.Context, request *connect.Request[octantv1alpha.GenerateManifestsRequest], stream *connect.ServerStream[octantv1alpha.GenerateManifestsResponse]) error {
+	logger := zap.L().With(zap.String("operation", octantv1alphaconnect.DatadogServiceGetDatadogIntegrationsProcedure))
+
+	buf, err := ch.compressor.CreateCompressed(ctx, connection.CompressionInput{
+		Namespace:   request.Msg.GetNamespace(),
+		Connection:  request.Msg.GetConnectionName(),
+		Telemetries: request.Msg.GetTelemetryTypes(),
+		Format:      request.Msg.GetFormat(),
+	})
+	if err != nil {
+		logger.Error("Failed to generate manfiest zip file", zap.Error(err))
+		return connect.NewError(connect.CodeInternal, errors.New("generate zip file"))
+	}
+
+	total := buf.Len()
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Info("Context cancelled, end transfer")
+			return nil
+		default:
+		}
+
+		chunk := make([]byte, chunkSize)
+		_, err := buf.Read(chunk)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			logger.Error("Failed to read chunks of zip", zap.Error(err))
+			return connect.NewError(connect.CodeInternal, errors.New("transferring zip"))
+		}
+		if err := stream.Send(&octantv1alpha.GenerateManifestsResponse{
+			Data:  chunk,
+			Total: uint64(total),
+			Type:  manifestContentZip,
+		}); err != nil {
+			logger.Error("Failed to send data chunk", zap.Error(err))
+			return connect.NewError(connect.CodeInternal, errors.New("streaming"))
+		}
+	}
+}
