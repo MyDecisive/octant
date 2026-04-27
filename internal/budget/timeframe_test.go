1635e6e59e03d375e9f5a8d438562eb5c35ec68a
xhsun <xhsun@users.noreply.github.com>
2026-04-27 14:47:50 +0000

    test

---
+++ internal/budget/timeframe_test.go
@@ -0,0 +1,46 @@
+package budget
+
+import (
+	"testing"
+	"time"
+
+	budgetv1alpha "github.com/MyDecisive/octant-contracts/go/pkg/budget/v1alpha"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidTimeframe(t *testing.T) {
+	t.Parallel()
+
+	var cases = []struct {
+		des      string
+		in       time.Time
+		expected budgetv1alpha.Timeframe
+	}{
+		{
+			"no valid",
+			time.Now(),
+			budgetv1alpha.Timeframe_TIMEFRAME_UNSPECIFIED,
+		},
+		{
+			"24h",
+			time.Now().Add(dayInHR + time.Hour),
+			budgetv1alpha.Timeframe_TIMEFRAME_24HR,
+		},
+		{
+			"month to date",
+			time.Now().Add(monthInHR + time.Hour),
+			budgetv1alpha.Timeframe_TIMEFRAME_MTD,
+		},
+		{
+			"last month",
+			time.Now().Add(2*monthInHR + time.Hour),
+			budgetv1alpha.Timeframe_TIMEFRAME_LM,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.des, func(t *testing.T) {
+			actual := ValidTimeframe(tt.in)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
