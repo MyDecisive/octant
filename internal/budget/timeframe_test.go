addd9ffb616aecfbc23f1b60511f1bc85d2723a0
Hannah Sun <xhsun@users.noreply.github.com>
2026-04-28 09:20:23 -0600

    feat: timeframe validator (#12)

---
+++ internal/budget/timeframe_test.go
@@ -0,0 +1,47 @@
+package budget
+
+import (
+	"testing"
+	"time"
+
+	budgetv1alpha "github.com/MyDecisive/octant-contracts/go/pkg/budget/v1alpha"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidTimeframe(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		des      string
+		in       time.Time
+		expected budgetv1alpha.Timeframe
+	}{
+		{
+			"no valid",
+			time.Now(),
+			budgetv1alpha.Timeframe_TIMEFRAME_UNSPECIFIED,
+		},
+		{
+			"24h",
+			time.Now().Add(-dayInHR),
+			budgetv1alpha.Timeframe_TIMEFRAME_24HR,
+		},
+		{
+			"month to date",
+			time.Now().Add(-monthInHR),
+			budgetv1alpha.Timeframe_TIMEFRAME_MTD,
+		},
+		{
+			"last month",
+			time.Now().Add(-(2 * monthInHR)),
+			budgetv1alpha.Timeframe_TIMEFRAME_LM,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.des, func(t *testing.T) {
+			t.Parallel()
+			actual := ValidTimeframe(tt.in)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
