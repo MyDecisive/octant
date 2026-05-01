eca7c7a9b6dc2bc9ab2dd7cb797d1efd4085d33b
xhsun <xhsun@users.noreply.github.com>
2026-05-01 16:42:12 +0000

    rename

--- internal/budget/data/metricRetriever.go
+++ internal/budget/data/metricRetriever.go
@@ -6,16 +6,16 @@ import (
 
 // Overall represents an overall metric data entry from the data store.
 type Overall struct {
-	LogReceived  uint64
-	LogSend      uint64
-	SpanReceived uint64
-	SpanSend     uint64
+	LogReceived  int64
+	LogSend      int64
+	SpanReceived int64
+	SpanSend     int64
 }
 
 // Log represents a single log data entry from the data store.
 type Log struct {
-	Name string
-	Sent uint64
+	Name   string
+	Amount int64
 }
 
 // RootSpan represents a single root span data entry from the data store.
@@ -24,7 +24,7 @@ type RootSpan struct {
 	Breath     uint32
 	Depth      uint32
 	Invocation uint32
-	Count      uint64
+	Count      int64
 }
 
 // MetricDataInput contains parameters needed to retrieve metric data.
@@ -41,7 +41,7 @@ type MetricDataRetriever interface {
 	// GetOverall returns the overall summary of the log and span data for the given timeframe.
 	GetOverall(timeframe budgetv1alpha.Timeframe, namespace string) (*Overall, error)
 	// GetTotalLog returns total amount of log data sent.
-	GetTotalLog(timeframe budgetv1alpha.Timeframe, namespace string) (uint64, error)
+	GetTotalLog(timeframe budgetv1alpha.Timeframe, namespace string) (int64, error)
 	// GetLogs returns the list of log data that matches the given inputs.
 	GetLogs(input MetricDataInput) ([]Log, error)
 	// GetRootSpans returns the list of root span data that matches the given inputs.
