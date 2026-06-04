0863889b1d4fa73582a861851ebebd199f1e93ba
Trent Vigar <trent@mydecisive.ai>
2026-06-04 14:35:27 -0400

    ENG-1352 checking if cm key exists first

--- internal/connection/octant.go
+++ internal/connection/octant.go
@@ -362,12 +362,14 @@ func (oc *OctantConnection) updateConnection(
 	connection OctantConnectionData,
 	namespace, connectionName string,
 ) error {
-	var existingConnection OctantConnectionData
-	if err := json.Unmarshal([]byte(cm.Data[connectionName]), &existingConnection); err != nil {
-		return fmt.Errorf("failed to unmarshal connection data: %w", err)
-	}
 	// ensure we're preserving the existing Created time.
-	connection.Created = existingConnection.Created
+	if _, ok := cm.Data[connectionName]; ok {
+		var existingConnection OctantConnectionData
+		if err := json.Unmarshal([]byte(cm.Data[connectionName]), &existingConnection); err != nil {
+			return fmt.Errorf("failed to unmarshal connection data: %w", err)
+		}
+		connection.Created = existingConnection.Created
+	}
 
 	jsonData, err := json.Marshal(connection)
 	if err != nil {
